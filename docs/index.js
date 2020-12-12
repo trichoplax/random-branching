cced44209a6b0034dda1df3b4a97975ae7034d9f
trichoplax <xalpohcirt@gmail.com>
2020-12-12 19:50:07 +0000

    Show all descendents in grey on mouseover

--- docs/index.js
+++ docs/index.js
@@ -1,8 +1,16 @@
 const EXPIRY_PROBABILITY = 0.1
+let mouseLocation = {x: 0, y: 0}
+let midpoint = {x: 0, y: 0}
+let offset = {x: 0, y: 0}
+let scale = 1
 
 document.addEventListener('DOMContentLoaded', () => {
   const canvas = document.getElementById('branching-canvas')
   const context = canvas.getContext('2d')
+
+  canvas.addEventListener('mousemove', event => {
+    mouseLocation = {x: event.offsetX, y: event.offsetY}
+  })
 
   const fitCanvas = fitCanvasMaker(canvas)
   fitCanvas()
@@ -17,6 +25,8 @@ const fitCanvasMaker = canvas => () => {
 
   canvas.width = computed.width.slice(0, -2)
   canvas.height = computed.height.slice(0, -2)
+
+  offset = {x: canvas.width / 2, y: canvas.height / 2}
 }
 
 const renderLoopMaker = (canvas, context) => {
@@ -25,10 +35,8 @@ const renderLoopMaker = (canvas, context) => {
       x: 0,
       y: 0,
     },
-    parent: {
-      x: 0,
-      y: 0,
-    },
+    parent: null,
+    children: [],
     colour: {
       red: 98,
       green: 98,
@@ -39,16 +47,8 @@ const renderLoopMaker = (canvas, context) => {
 
   let liveCentres = [...centres]
 
-  let midX = 0
-  let midY = 0
-  let scale = 1
-
   return function renderLoop() {
-    const newCentre = attemptNewCentre(centres, liveCentres)
-    if (newCentre !== null) {
-      centres.push(newCentre)
-      liveCentres.push(newCentre)
-    }
+    attemptNewCentre(centres, liveCentres)
 
     let width = canvas.width
     let height = canvas.height
@@ -64,11 +64,11 @@ const renderLoopMaker = (canvas, context) => {
     let yScale = height / (maxY - minY)
     let scaleTarget = Math.min(xScale, yScale)
 
-    let xOffset = width / 2
-    let yOffset = height / 2
+    midpoint = {
+      x: approach(midpoint.x, midXTarget),
+      y: approach(midpoint.y, midYTarget),
+    }
 
-    midX = approach(midX, midXTarget)
-    midY = approach(midY, midYTarget)
     scale = approach(scale, scaleTarget)
 
     context.strokeStyle = 'white'
@@ -76,77 +76,97 @@ const renderLoopMaker = (canvas, context) => {
 
     context.clearRect(0, 0, width, height)
 
-    for (let centre of centres) {
-      adjustedX = (centre.location.x - midX) * scale + xOffset
-      adjustedY = (centre.location.y - midY) * scale + yOffset
-      adjustedParentX = (centre.parent.x - midX) * scale + xOffset
-      adjustedParentY = (centre.parent.y - midY) * scale + yOffset
-
-      if (centre.expired == 0) {
-        context.fillStyle = 'rgb(196, 255, 150)'
-      } else {
-        let colour = centre.colour
-        context.fillStyle = `rgb(
-          ${colour.red},
-          ${colour.green},
-          ${colour.blue}
-        )`
-      }
-
-      context.beginPath()
-      context.arc(
-        adjustedX,
-        adjustedY,
-        scale,
-        0,
-        2 * Math.PI
-      )
-      context.closePath()
-      context.fill()
-
-      context.fillStyle = 'white'
-      context.beginPath()
-      context.arc(
-        adjustedX,
-        adjustedY,
-        scale / 4,
-        0,
-        2 * Math.PI
-      )
-      context.closePath()
-      context.fill()
-
-      context.beginPath()
-      context.moveTo(adjustedX, adjustedY)
-      context.lineTo(adjustedParentX, adjustedParentY)
-      context.stroke()
-    }
+    drawCircleAndDescendants(context, centres[0], false)
 
     window.requestAnimationFrame(renderLoop)
   }
 }
 
-const attemptNewCentre = (centres, liveCentres) => {
-  let candidate = null
+const drawCircleAndDescendants = (context, centre, ancestorSelected) => {
+  let adjustedLocation = adjusted(centre.location)
+  let affected = containsMouse(adjustedLocation, scale) || ancestorSelected
 
+  if (affected) {
+    context.fillStyle = 'grey'
+  } else if (centre.expired == 0) {
+    context.fillStyle = 'rgb(196, 255, 150)'
+  } else {
+    let colour = centre.colour
+    context.fillStyle = `rgb(
+      ${colour.red},
+      ${colour.green},
+      ${colour.blue}
+    )`
+  }
+
+  context.beginPath()
+  context.arc(
+    adjustedLocation.x,
+    adjustedLocation.y,
+    scale,
+    0,
+    2 * Math.PI
+  )
+  context.closePath()
+  context.fill()
+
+  context.fillStyle = 'white'
+  context.beginPath()
+  context.arc(
+    adjustedLocation.x,
+    adjustedLocation.y,
+    scale / 4,
+    0,
+    2 * Math.PI
+  )
+  context.closePath()
+  context.fill()
+
+  if (centre.parent) {
+    let adjustedParentLocation = adjusted(centre.parent.location)
+    context.beginPath()
+    context.moveTo(adjustedLocation.x, adjustedLocation.y)
+    context.lineTo(adjustedParentLocation.x, adjustedParentLocation.y)
+    context.stroke()
+  }
+
+  for (let child of centre.children) {
+    drawCircleAndDescendants(context, child, affected)
+  }
+}
+
+const adjusted = (location) => ({
+  x: (location.x - midpoint.x) * scale + offset.x,
+  y: (location.y - midpoint.y) * scale + offset.y,
+})
+
+const containsMouse = (location, scale) => {
+  if (distance_squared(mouseLocation, location) < scale ** 2) {
+    return true
+  }
+  return false
+}
+
+const attemptNewCentre = (centres, liveCentres) => {
   if (liveCentres.length > 0) {
     const index = Math.floor(Math.random() * liveCentres.length)
     const potentialParent = liveCentres[index]
 
     const angle = Math.random() * 2 * Math.PI
 
-    let expired = 0
-    let colour = driftColour(potentialParent.colour)
-    let potentialParentX = potentialParent.location.x
-    let potentialParentY = potentialParent.location.y
-    let parent = {x: potentialParentX, y: potentialParentY}
-    let x = potentialParentX + Math.cos(angle) * 2
-    let y = potentialParentY + Math.sin(angle) * 2
+    let x = potentialParent.location.x + Math.cos(angle) * 2
+    let y = potentialParent.location.y + Math.sin(angle) * 2
     let location = {x, y}
-    candidate = {location, parent, colour, expired}
 
-    if (overlap(centres, candidate)) {
-      candidate = null
+    if (!overlap(centres, location)) {
+      let expired = 0
+      let colour = driftColour(potentialParent.colour)
+      let children = []
+      let parent = potentialParent
+      let candidate = {location, parent, children, colour, expired}
+      centres.push(candidate)
+      liveCentres.push(candidate)
+      potentialParent.children.push(candidate)
     }
 
     if (Math.random() < EXPIRY_PROBABILITY) {
@@ -154,13 +174,11 @@ const attemptNewCentre = (centres, liveCentres) => {
       liveCentres.splice(index, 1)
     }
   }
-
-  return candidate
 }
 
-const overlap = (centres, candidate) => {
+const overlap = (centres, location) => {
   for (let centre of centres) {
-    if (distance_squared(centre.location, candidate.location) < 4) {
+    if (distance_squared(centre.location, location) < 4) {
       return true
     }
   }
