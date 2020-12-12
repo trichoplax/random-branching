0be78e9f0c90115ec3079b589953d6477e9bc102
trichoplax <xalpohcirt@gmail.com>
2020-12-12 01:18:03 +0000

    Place circle on canvas using javascript

--- docs/index.js
+++ docs/index.js
@@ -1,2 +1,22 @@
-console.log('Nothing here yet')
+document.addEventListener('DOMContentLoaded', () => {
+  const canvas = document.getElementById('branching-canvas')
+  const context = canvas.getContext('2d')
+
+  let computed = getComputedStyle(canvas)
+
+  canvas.width = computed.width.slice(0, -2)
+  canvas.height = computed.height.slice(0, -2)
+
+  let width = canvas.width
+  let height = canvas.height
+
+  context.arc(
+    width / 2,
+    height / 2,
+    Math.min(width, height) / 2,
+    0,
+    2 * Math.PI
+  )
+  context.fill()
+})
 
