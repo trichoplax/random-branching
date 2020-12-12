721dbe1e88b4c6ec6587f7921737fa9099367a54
trichoplax <xalpohcirt@gmail.com>
2020-12-12 03:25:56 +0000

    Ensure added circles never overlap

--- docs/index.js
+++ docs/index.js
@@ -24,35 +24,38 @@ const renderLoopMaker = (canvas, context) => {
     newCentre = attemptNewCentre(centres)
     if (newCentre !== null) {
       centres.push(newCentre)
-    }
 
-    let width = canvas.width
-    let height = canvas.height
+      let width = canvas.width
+      let height = canvas.height
 
-    let minX = Math.min(...centres.map(centre => centre[0])) - 1
-    let maxX = Math.max(...centres.map(centre => centre[0])) + 1
-    let midX = (minX + maxX) / 2
-    let minY = Math.min(...centres.map(centre => centre[1])) - 1
-    let maxY = Math.max(...centres.map(centre => centre[1])) + 1
-    let midY = (minY + maxY) / 2
+      let minX = Math.min(...centres.map(centre => centre[0])) - 1
+      let maxX = Math.max(...centres.map(centre => centre[0])) + 1
+      let midX = (minX + maxX) / 2
+      let minY = Math.min(...centres.map(centre => centre[1])) - 1
+      let maxY = Math.max(...centres.map(centre => centre[1])) + 1
+      let midY = (minY + maxY) / 2
 
-    xScale = width / (maxX - minX)
-    yScale = height / (maxY - minY)
-    scale = Math.min(xScale, yScale)
+      xScale = width / (maxX - minX)
+      yScale = height / (maxY - minY)
+      scale = Math.min(xScale, yScale)
 
-    xOffset = width / 2
-    yOffset = height / 2
+      xOffset = width / 2
+      yOffset = height / 2
 
-    for (let centre of centres) {
-      context.arc(
-        (centre[0] - midX) * scale + width / 2,
-        (centre[1] - midY) * scale + height / 2,
-        scale,
-        0,
-        2 * Math.PI
-      )
-      context.closePath()
-      context.fill()
+      context.clearRect(0, 0, width, height)
+
+      for (let centre of centres) {
+        context.beginPath()
+        context.arc(
+          (centre[0] - midX) * scale + width / 2,
+          (centre[1] - midY) * scale + height / 2,
+          scale,
+          0,
+          2 * Math.PI
+        )
+        context.closePath()
+        context.fill()
+      }
     }
 
     window.requestAnimationFrame(renderLoop)
@@ -62,6 +65,24 @@ const renderLoopMaker = (canvas, context) => {
 const attemptNewCentre = centres => {
   let x = Math.random() * 10 - 5
   let y = Math.random() * 10 - 5
-  return [x, y]
+  candidate = [x, y]
+  if (overlap(centres, candidate)) {
+    candidate = null
+  }
+  return candidate
+}
+
+const overlap = (centres, candidate) => {
+  for (let centre of centres) {
+    if (distance_squared(centre, candidate) < 4) {
+      return true
+    }
+  }
+
+  return false
+}
+
+const distance_squared = (a, b) => {
+  return (b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2
 }
 
